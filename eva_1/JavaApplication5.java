27a52e64f34b8ca4c5aecc6d8500b03a5ec14058
Gatomaxxpro <143568715+Gatomaxxpro@users.noreply.github.com>
2023-09-12 00:49:54 -0700

    Add files via upload

---
+++ eva_1/JavaApplication5.java
@@ -0,0 +1,31 @@
+
+package javaapplication5;
+
+
+public class JavaApplication5 {
+
+   
+    public static void main(String[] args) {
+        // TODO CODE APPLICATION LOGIC HERE ASJDSKADSAJSJASJA
+        //;-->fin de instruccion
+        //java es case sensitive (sensible alas mayusculas  y minusculas)
+        //NO ES LO MISMO String que string
+        String marca;
+        String modelo;
+        int anio;
+        // hay un paquete System, dentro un paquete out, y dentro 
+        // de out , la instruccion println
+        System.out.println("introduce la marca del auto");
+
+        System.out.println("introduce el modelo del auto");
+
+        System.out.println("introduce el año del auto");
+        
+    
+        
+        
+        
+        
+    }
+    
+}
